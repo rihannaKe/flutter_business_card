5519ad6c378ac39af1bb1da4067146429fae7663
rihannaKe <rihanna.ke@gmail.com>
2020-01-05 01:16:03 +0100

    added card border

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,38 @@ class MyApp extends StatelessWidget {
 }
 
 class MyBusinessCard extends StatelessWidget {
+  Gradient backgroundGradientColor = LinearGradient(
+// Where the linear gradient begins and ends
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+// Add one stop for each color. Stops should increase from 0 to 1
+    stops: const [0.1, 0.4, 0.7, 0.9],
+    colors: [
+      Colors.white.withOpacity(1),
+      Colors.white.withOpacity(0.97),
+      Colors.white.withOpacity(0.90),
+      Colors.white.withOpacity(0.86),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: MediaQuery.of(context).size.width < 600
-            ? _buildXSLayout()
-            : _buildNotXSLayout());
+        child: Container(
+            margin: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                const BoxShadow(
+                    color: Colors.black26, offset: Offset(0, 0), blurRadius: 24)
+              ],
+              gradient: backgroundGradientColor,
+            ),
+            child: MediaQuery.of(context).size.width < 600
+                ? _buildXSLayout()
+                : _buildNotXSLayout()
+        ),
+    );
   }
 
   Widget _buildXSLayout() {
@@ -49,14 +75,17 @@ class MyBusinessCard extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Expanded(flex: 5, child: _mainInfo()),
-        Expanded(flex: 1, child: VerticalDivider(
-          width: 40.0,
-          indent: 40.0,
-          endIndent: 40.0,
-          thickness: 3.0,
-          color: kDividerColor,
-        ),),
-        Expanded( flex: 7, child: _socialLinkInfo()),
+        Expanded(
+          flex: 1,
+          child: VerticalDivider(
+            width: 40.0,
+            indent: 40.0,
+            endIndent: 40.0,
+            thickness: 3.0,
+            color: kDividerColor,
+          ),
+        ),
+        Expanded(flex: 7, child: _socialLinkInfo()),
       ],
     );
   }
@@ -80,7 +109,7 @@ class MyBusinessCard extends StatelessWidget {
   }
 
   Widget _socialLinkInfo() {
-    return  ListView(
+    return ListView(
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       children: <Widget>[
@@ -90,7 +119,7 @@ class MyBusinessCard extends StatelessWidget {
               leading: Icon(FontAwesomeIcons.twitter, color: kIconColors),
               title: Text('@rihanna_ke', style: kSocialTextStyle),
               onTap: () {
-                  UrlUtils.open('https://twitter.com/rihanna_ke');
+                UrlUtils.open('https://twitter.com/rihanna_ke');
               },
             )),
         Card(
@@ -99,8 +128,8 @@ class MyBusinessCard extends StatelessWidget {
               leading: Icon(FontAwesomeIcons.linkedin, color: kIconColors),
               title: Text('rihanna-kedir', style: kSocialTextStyle),
               onTap: () {
-                  UrlUtils.open(
-                      'https://www.linkedin.com/in/rihanna-kedir-05068568/');
+                UrlUtils.open(
+                    'https://www.linkedin.com/in/rihanna-kedir-05068568/');
               },
             )),
         Card(
@@ -109,7 +138,7 @@ class MyBusinessCard extends StatelessWidget {
               leading: Icon(FontAwesomeIcons.github, color: kIconColors),
               title: Text('rihannaKe', style: kSocialTextStyle),
               onTap: () {
-                  UrlUtils.open('https://github.com/rihannaKe');
+                UrlUtils.open('https://github.com/rihannaKe');
               },
             )),
         Card(
@@ -118,22 +147,20 @@ class MyBusinessCard extends StatelessWidget {
               leading: Icon(FontAwesomeIcons.globeAfrica, color: kIconColors),
               title: Text('www.rihannakedir.dev', style: kSocialTextStyle),
               onTap: () {
-                  UrlUtils.open('https://rihannakedir.dev');
+                UrlUtils.open('https://rihannakedir.dev');
               },
             )),
         Card(
             margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
             child: ListTile(
               leading: Icon(FontAwesomeIcons.envelopeOpen, color: kIconColors),
-              title: Text(
-                  'rihanna.kedir.dev[at]gmail.com', style: kSocialTextStyle),
+              title: Text('rihanna.kedir.dev[at]gmail.com',
+                  style: kSocialTextStyle),
               onTap: () {
-                  UrlUtils.sendEmail('rihanna.kedir.dev@gmail.com');
+                UrlUtils.sendEmail('rihanna.kedir.dev@gmail.com');
               },
-            )
-        ),
+            )),
       ],
     );
   }
-
 }
